5cfa35ba0010d4558415a3dbf49ceca029195788
bwilkins96 <ben.jr.speed@gmail.com>
2023-05-12 09:53:46 -0400

    responses

--- teams.py
+++ teams.py
@@ -1,19 +1,5 @@
-# 1) Add the __contains__ protocol and show whether or not  'Tim' and 'Sam' are part of our team.  
-
-# 2) Add the __iter__ protocol and show how you can print each member of the classmates object.
-
-# 3) Determine if the class classmates implements the __len__ method.
-
-# 4) Explain the difference between interfaces and implementation. 
-
-# 5) Using both visual and written descriptions, think through the interface-implementation of
-# a large scale storage system. In many systems today, we have the ability to store information
-# from a single application to a variety of storage devices - local storage (hard drive, usb), 
-# the cloud and/or some new medium in the future.
-#  
-# How would you design an interface structure such that all of the possible 
-# implementations could store data effectively?
-
+# SWDV 630 - Object-Oriented Software Architecture
+# Added __contains__ and __iter__ to Teams class
 
 class Teams:
   def __init__(self, members):
@@ -40,5 +26,3 @@ def main():
     print('Hello from', member)
 
 if __name__ == '__main__': main()
-
-# 3. The Teams class does implement the __len__ method.
